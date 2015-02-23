41e5124a0ff6ba342f504e26e9bf3b3061afd4c7
Matt Ankerson <ankem1@student.op.ac.nz>
2015-02-23 21:29:00 +1300

    started blackjack lab

--- blackjack/Card.py
+++ blackjack/Card.py
@@ -1,1 +1,18 @@
-__author__ = 'Matty'
+# Card class
+# Author: Matthew Ankerson
+# Date: 23 Feb 2015
+# Holds information relevant to a single Card in a Deck.
+
+
+class Card():
+    suit = None
+    value = None
+
+    # Default constructor
+    def __init__(self, suit, value):
+        self.suit = suit
+        self.value = value
+
+    def card(self, suit, value):
+        self.suit = suit
+        self.value = value
--- blackjack/Deck.py
+++ blackjack/Deck.py
@@ -1,1 +1,19 @@
-__author__ = 'Matty'
+import Card
+# Deck class
+# Author: Matthew Ankerson
+# Date: 23 Feb 2015
+# This class holds cards and performs functions necessary for a Deck
+
+
+class Deck():
+    cards = None
+
+    # Default constructor
+    # Create the Deck of Cards
+    #def __init__(self):
+
+    # Create a complete deck of Cards
+    def makecards(self):
+        # Loop over the 52 Cards
+        for i in xrange(52):
+            c = Card.Card(1, (i + 1))     # Create a new Card

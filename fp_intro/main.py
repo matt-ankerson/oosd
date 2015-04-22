e02050cbe8029d92d2faca7226a0e54a670bda7b
Matthew Ankerson <sm.ankerson@gmail.com>
2015-04-22 14:44:07 +1200

    solved q4 with list compreension, revise this later

--- fp_intro/main.py
+++ fp_intro/main.py
@@ -2,7 +2,7 @@
 # Date: 22 April 2015
 
 # Write a lambda expression that computes factorials. (Use reduce)
-reduce(lambda x, y: x * y, range(1, 6))
+print(reduce(lambda x, y: x * y, range(1, 6)))
 
 
 # Write a function that takes a string and a character and returns a new string that is the
@@ -11,14 +11,18 @@ def remove_letter(c="h", st="hello world"):
     return filter(lambda x: x != c, st)
 
 remove_a_letter = lambda st, c: filter(lambda x: x != c, st)
+print(remove_a_letter("hello world", "o"))
 
 # Write a function that is similar to the one above,
 # but that returns the number of occurences of the character.
 count_occurences = lambda st, c: len(filter(lambda x: x == c, st))
+print(count_occurences("hello world", "o"))
 
 
 # Write a function that takes a string and a character and returns the number of words that start with the character.
-
+def count_words(st, c):
+    return len([word for word in st.split() if word.startswith(c)])
+print(count_words("cracker crunch monday", "c"))
 
 # Write a function that takes a string and a character and returns a new string
 # that is the original string with all occurences of the character in uppercase.

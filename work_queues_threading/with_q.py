c12fb476c24064fe22d52ad7bc7a97cb8e050d18
Matthew Ankerson <sm.ankerson@gmail.com>
2015-05-01 22:51:26 +1200

    with_q multithreading working, though still untidy

--- work_queues_threading/with_q.py
+++ work_queues_threading/with_q.py
@@ -10,26 +10,24 @@ class PingCheck(threading.Thread):
         self.out_q = out_q
         self.daemon = True  # ensure that our threads are destroyed when the main finishes
 
-    def run(self):
-        while True:
-            ip_address = self.in_q.get()
-            print(ip_address)
-            ping_out = os.popen("ping -q -c2 " + ip, "r")
-            line = ping_out.readline()
-            n_received = re.findall(self.received_packages,line)
-            result = "IP address {0}: {1} received".format(ip, n_received)
-            self.out_q.put(result)
-            self.in_q.task_done()
-
     def do_ping(self, ip_address):
         ping_out = os.popen("ping -q -c2 " + ip_address, "r")
         while True:
             line = ping_out.readline()
             if not line:
                 break
-            n_recieved = ""
-            result = ""
+            n_received = re.findall(self.received_packages, line)
+            if n_received:
+                result = "IP address {0}: {1} received".format(ip, n_received)
         return result
+
+    def run(self):
+        while True:
+            ip_address = self.in_q.get()
+            result = self.do_ping(ip_address)
+            print(result)
+            self.out_q.put(result)
+            self.in_q.task_done()
 
 
 class PingResult(threading.Thread):
@@ -42,9 +40,8 @@ class PingResult(threading.Thread):
 
     def run(self):
         while True:
-            result = self.out_q.get()
             self.lock.acquire()
-            print(result)
+            print(self.out_q.get())
             self.lock.release()
             self.out_q.task_done()
 
@@ -52,19 +49,19 @@ if __name__ == "__main__":
     in_queue = Queue.Queue()
     out_queue = Queue.Queue()
 
+    # spawn processes that will process the output in out_q
+    for n in xrange(10):
+        get_result = PingResult(out_queue)
+        get_result.start()
+
     # put all the work items in the in_queue
     for suffix in range(1,255):
-        ip = "10.25.1."+str(suffix)
+        ip = "192.168.1."+str(suffix)
         in_queue.put(ip)
 
     # spawn processes that will process the work items and put results in the out_queue
     for n in xrange(10):
         check = PingCheck(in_queue, out_queue)
         check.start()
-
-    # spawn processes that will process the output in out_q
-    for n in xrange(10):
-        get_result = PingResult(out_queue)
-        get_result.start()
 
     out = raw_input()  # exit the program and kill threads

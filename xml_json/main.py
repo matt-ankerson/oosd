f4d9d313db7289870e478dce11aed50f37a06d6f
Matthew Ankerson <sm.ankerson@gmail.com>
2015-05-08 09:47:57 +1200

    reading xml, still need to implement json

--- xml_json/main.py
+++ xml_json/main.py
@@ -1,4 +1,29 @@
+from doc_factory import DocumentFactory
+
 # Author: Matt Ankerson
 # Date: 7 May 2015
 
 
+def print_dictionary(dictionary, indent="", braces=1):
+    for key, value in dictionary.iteritems():
+        if isinstance(value, dict):
+            print '%s%s%s%s' % (indent, braces * '[', key, braces * ']')
+            print_dictionary(value, indent + '  ', braces + 1)
+        else:
+            print indent + "%s = %s" % (key, value) + '\n'
+
+if __name__ == "__main__":
+    # get the file name/path
+    file_name = raw_input("Filename: ")
+
+    # create the document factory
+    df = DocumentFactory()
+
+    # create the appropriate document
+    doc = df.create_document(file_name)
+
+    # get the dictionary representation from the document
+    dic = doc.doc
+
+    # print the dictionary
+    print_dictionary(dic)
--- xml_json/xml_doc.py
+++ xml_json/xml_doc.py
@@ -12,6 +12,15 @@ class XmlDoc(Document):
         self.file_path = file_path
         self.doc = self.read()
 
+    # This function only pulls tags and attributes from xml, not inner text. Need to fix this.
+    def tree_to_dict(self, tree):
+        # map returns a new list produced by applying the given function to each element in the given list
+        dic = {tree.tag: map(self.tree_to_dict, tree.getchildren())}
+        dic.update((k, v) for k, v in tree.attrib.iteritems())  # <<-- we also need the inner text.
+        return dic
+
     def read(self):
-        xml_doc = {}
+        tree = ET.parse(self.file_path)
+        print(self.file_path)
+        xml_doc = self.tree_to_dict(tree.getroot())
         return xml_doc
